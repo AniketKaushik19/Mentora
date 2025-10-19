5f805a6ae84c2408d1a1bf5489e39b86ee8ead5f
AniketKaushik19 <aniketsskaushik@gmail.com>
2025-10-19 15:05:24 +0530

    Navbar completed

--- convex/schema.js
+++ convex/schema.js
@@ -4,6 +4,19 @@ import {v} from 'convex/values'
 export default defineSchema({
     users:defineTable({
         name:v.string(),
-        tokenIdentifier:v.string(),
-    }).index("by_token", ["tokenIdentifier"]),
+        email: v.optional(v.string()),
+        imageUrl:v.optional(v.string()),
+        username:v.optional(v.string()),
+        tokenIdentifier:v.string(),  //clerk user ID for auth
+
+        //Activity timestamps
+        lastActiveAt:v.optional(v.float64(Date.now())),
+    })
+    .index("by_token", ["tokenIdentifier"])
+    .index("by_email",["email"])  //email lookup
+    .index("by_username", ["username"]) //username lookup for public profile
+
+    .searchIndex("search_name" ,{searchField:"name"}) //user search
+    .searchIndex("search_email" ,{searchField:"email"}),
+
 })
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-       <Button variant={'primary'}>hello subcribe gameer kinnng</Button>
+       
     </div>
   );
 }
