68f838e1628b86b6bef2ab84f9d6a9fbb79738e2
SoniaSharma15 <148994808+SoniaSharma15@users.noreply.github.com>
2025-10-19 16:32:14 +0530

    Save my local changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import WelcomeBanner from "./dashboard/_components/WelcomeBanner";
-import AItools from "./dashboard/_components/AItools";
-import PrevHistory from "./dashboard/_components/PrevHistory";
+
 export default function Home() {
   return (
-    <div className="mx-1 md:px-20 my-32">
-    <WelcomeBanner/>
-    <AItools/>
-    <PrevHistory/>
+    <div>
+       <Button variant={'primary'}>hello subcribe gameer kinnng</Button>
     </div>
   );
 }
